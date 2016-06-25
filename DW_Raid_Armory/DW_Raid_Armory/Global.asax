3365c32d5a54344e33e56ea791bd38b020eed2cc
Forced1988 <menno.van.scheers@gmail.com>
2016-06-25 16:19:41 +0200

    Initial

---
+++ DW_Raid_Armory/DW_Raid_Armory/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="DW_Raid_Armory.MvcApplication" Language="C#" %>
---
+++ DW_Raid_Armory/DW_Raid_Armory/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(DW_Raid_Armory.Startup))]
+namespace DW_Raid_Armory
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
